4e143b357b7625fbb668ff558c6f42efd7dd455e
Latisha Bridges <msbridges3368@gmail.com>
2023-04-17 10:44:22 -0400

    first commit

---
+++ hasMoreVowels.js
@@ -0,0 +1,24 @@
+function hasMoreVowels(word) {
+    const vowels = ['a', 'e', 'i', 'o', 'u']
+    let vowelCount = 0
+    let nonVowelCount = 0
+
+    for (let i = 0; i < word.length; i++) {
+    if (vowels.includes(word[i].toLowerCase())) {
+      vowelCount++
+    } else {
+      nonVowelCount++
+    }
+  }
+
+  const moreVowels = vowelCount > nonVowelCount
+  const isOverHalfVowels = vowelCount / word.length > 0.5
+
+  const result =  moreVowels && isOverHalfVowels
+  
+  return result
+}
+console.log(hasMoreVowels('plum'))
+console.log(hasMoreVowels('developer'))
+console.log(hasMoreVowels('engineering'))
+console.log(hasMoreVowels('ocean'))
